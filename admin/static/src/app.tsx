9b2e9967b637d08de6442e9ef7e7aab49a958324
luxiaofeng <lxfljw@gmail.com>
2020-09-05 10:45:35 +0800

    test✅ 测试pre-commit

--- admin/static/src/app.tsx
+++ admin/static/src/app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./app.scss";
 import { Switch, HashRouter as Router, Link, Route } from "react-router-dom";
 import Home from "@/pages/home";
+// impoer a from '../ee'
 export default function App() {
   return (
     <div className="app">
@@ -11,6 +12,7 @@ export default function App() {
           <Route path="/home" component={Home}></Route>
           <Route exact path="/">
             {" "}
+            <p>这是App组件!</p>
             <p>这是App组件!</p>
             <p>这是APP2</p>
           </Route>
